266731d90b09c6dcc8d5eb929a6b77a4e4cf5ad6
unknown <dasavage1412@gmail.com>
2021-05-08 22:41:12 -0700

    updating APP.js file

---
+++ app.js
@@ -0,0 +1,39 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using D3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // Clear existing data
+    tbody.html("");
+
+    // Loop through objects and append row and cell for each value in the row
+    data.forEach((dataRow) => {
+        // Append a row to the table body
+        let row = tbody.append("tr");
+
+        // Loop through each row
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    }
+    );
+});
+
+function handleClick () {
+    let date = d3.select("#datetime").property(#value);
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+}
